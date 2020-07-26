75e32aa640a7c764ee8aaf6360eb484b77f18b50
TY Chen <tychen742@gmail.com>
2020-07-26 17:16:39 -0400

    changed title to Simple Todo

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.get('/', function (req, res) {
     <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=5">
-      <title>The Todo App</title>
+      <title> Simple Todo </title>
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
     </head>
     <body class="mw-50">
